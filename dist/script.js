99493df12149b829112c3a92bc3d0e5f74195fab
Leonid <hristenokleonid@gmail.com>
2026-09-24 00:06:47 +0300

    Add forms and forms validation functional

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,237 @@
-(()=>{"use strict";const e=setTimeout(()=>n(".popup-consultation"),6e4);let t;function n(t){const n=window.innerWidth-document.documentElement.clientWidth;o(),document.querySelector(t).classList.add("show","animated","fadeIn"),document.body.classList.add("modal-open"),document.body.style.marginRight=`${n}px`,clearTimeout(e)}function o(){document.querySelectorAll("[data-modal]").forEach(e=>{var t;t=`.${e.dataset.modal}`,document.body.style.marginRight="0px",document.querySelector(t).classList.remove("show","animated","fadeIn"),document.body.classList.remove("modal-open")})}window.addEventListener("scroll",()=>{!t&&window.scrollY+document.documentElement.clientHeight>=document.documentElement.scrollHeight-1&&document.querySelector(".fixed-gift").click()});const d=(e,d,i,c=!1)=>{const l=document.querySelectorAll(e),s=document.querySelector(d),a=document.querySelector(i);l.forEach(e=>{e.addEventListener("click",o=>{o.target&&(o.preventDefault(),n(d),t=!0,c&&e.remove())})}),a.addEventListener("click",()=>o()),s.addEventListener("click",e=>{e.target===s&&o()})},i=(e,t,n,o)=>{const d=document.querySelectorAll(e);let i,c=1,l=!1;function s(e){return e[0].toUpperCase()+e.slice(1)}function a(e,t){if(l)return;l=!0;const n=d[c-1];"right"!==e&&"up"!==e||(c++,c>d.length&&(c=1)),"left"!==e&&"down"!==e||(c--,c<1&&(c=d.length));const o=d[c-1];o.classList.remove(`slideOut${s(e)}`,`slideOut${s(t)}`,`slideIn${s(t)}`),n.classList.add(`slideOut${s(t)}`),o.classList.add(`slideIn${s(e)}`),o.style.display="block",n.addEventListener("animationend",()=>l=!1,{once:!0})}!function(e){let t=0;d.forEach(e=>{t=Math.max(t,e.offsetHeight),e.classList.add("animated"),e.style.display="none",e.style.position="absolute"}),d[0].parentNode.style.minHeight=t+"px",d[e-1].style.display="block"}(c),r();try{const e=document.querySelector(n),t=document.querySelector(o);e.addEventListener("click",()=>{a("left","right")}),t.addEventListener("click",()=>{a("right","left")})}catch(e){}function r(){i="vertical"===t?setInterval(()=>{a("up","up")},3e3):setInterval(()=>{a("right","left")},3e3)}d[0].parentNode.addEventListener("mouseenter",()=>{clearInterval(i)}),d[0].parentNode.addEventListener("mouseleave",()=>{r()})};window.addEventListener("DOMContentLoaded",()=>{d(".button-design",".popup-design",".popup-design .popup-close"),d(".button-consultation",".popup-consultation",".popup-consultation .popup-close"),d(".fixed-gift",".popup-gift",".popup-gift .popup-close",!0),i(".feedback-slider-item","",".main-prev-btn",".main-next-btn"),i(".main-slider-item","vertical")})})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/modules/modal.js"
+/*!*********************************!*\
+  !*** ./src/js/modules/modal.js ***!
+  \*********************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const modalTimerId = setTimeout(() => openModal('.popup-consultation'), 60000);
+let btnPressed;
+openModalByScroll('.fixed-gift');
+const modals = (triggerSelector, modalSelector, closeSelector, destroy = false) => {
+  const trigger = document.querySelectorAll(triggerSelector),
+    modal = document.querySelector(modalSelector),
+    close = document.querySelector(closeSelector);
+  trigger.forEach(item => {
+    item.addEventListener("click", e => {
+      if (e.target) {
+        e.preventDefault();
+        openModal(modalSelector);
+        btnPressed = true;
+        if (destroy) {
+          item.remove();
+        }
+      }
+    });
+  });
+  close.addEventListener("click", () => closeAllModals());
+  modal.addEventListener("click", e => {
+    if (e.target === modal) {
+      closeAllModals();
+    }
+  });
+};
+function openModal(modalSelector) {
+  const scrollWidth = window.innerWidth - document.documentElement.clientWidth;
+  closeAllModals();
+  document.querySelector(modalSelector).classList.add('show', 'animated', 'fadeIn');
+  document.body.classList.add('modal-open');
+  document.body.style.marginRight = `${scrollWidth}px`;
+  clearTimeout(modalTimerId);
+}
+function openModalByScroll(selector) {
+  window.addEventListener("scroll", () => {
+    if (!btnPressed && window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+      document.querySelector(selector).click();
+    }
+  });
+}
+function closeModal(modalSelector) {
+  document.body.style.marginRight = '0px';
+  document.querySelector(modalSelector).classList.remove('show', 'animated', 'fadeIn');
+  document.body.classList.remove('modal-open');
+}
+function closeAllModals() {
+  const modalsArr = document.querySelectorAll('[data-modal]');
+  modalsArr.forEach(item => {
+    closeModal(`.${item.dataset.modal}`);
+  });
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (modals);
+
+/***/ },
+
+/***/ "./src/js/modules/sliders.js"
+/*!***********************************!*\
+  !*** ./src/js/modules/sliders.js ***!
+  \***********************************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const sliders = (slide, dir, prev, next) => {
+  const slidesArr = document.querySelectorAll(slide);
+  let slideIndex = 1,
+    isAnimating = false,
+    intervalId;
+  initSlides(slideIndex);
+  setAnimation();
+  function initSlides(n) {
+    let height = 0;
+    slidesArr.forEach(slide => {
+      height = Math.max(height, slide.offsetHeight);
+      slide.classList.add('animated');
+      slide.style.display = 'none';
+      slide.style.position = 'absolute';
+    });
+    slidesArr[0].parentNode.style.minHeight = height + 'px';
+    slidesArr[n - 1].style.display = 'block';
+  }
+  function makeBigFirstLetter(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+  function showSlides(from, to) {
+    if (isAnimating) return;
+    isAnimating = true;
+    const currentSlide = slidesArr[slideIndex - 1];
+    if (from === 'right' || from === 'up') {
+      slideIndex++;
+      if (slideIndex > slidesArr.length) {
+        slideIndex = 1;
+      }
+    }
+    if (from === 'left' || from === 'down') {
+      slideIndex--;
+      if (slideIndex < 1) {
+        slideIndex = slidesArr.length;
+      }
+    }
+    const nextSlide = slidesArr[slideIndex - 1];
+    nextSlide.classList.remove(`slideOut${makeBigFirstLetter(from)}`, `slideOut${makeBigFirstLetter(to)}`, `slideIn${makeBigFirstLetter(to)}`);
+    currentSlide.classList.add(`slideOut${makeBigFirstLetter(to)}`);
+    nextSlide.classList.add(`slideIn${makeBigFirstLetter(from)}`);
+    nextSlide.style.display = 'block';
+    currentSlide.addEventListener('animationend', () => isAnimating = false, {
+      once: true
+    });
+  }
+  try {
+    const prevBtn = document.querySelector(prev),
+      nextBtn = document.querySelector(next);
+    prevBtn.addEventListener("click", () => {
+      showSlides('left', 'right');
+    });
+    nextBtn.addEventListener("click", () => {
+      showSlides('right', 'left');
+    });
+  } catch (e) {}
+  function setAnimation() {
+    if (dir === 'vertical') {
+      intervalId = setInterval(() => {
+        showSlides('up', 'up');
+      }, 3000);
+    } else {
+      intervalId = setInterval(() => {
+        showSlides('right', 'left');
+      }, 3000);
+    }
+  }
+  slidesArr[0].parentNode.addEventListener("mouseenter", () => {
+    clearInterval(intervalId);
+  });
+  slidesArr[0].parentNode.addEventListener("mouseleave", () => {
+    setAnimation();
+  });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (sliders);
+
+/***/ }
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	const __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		const cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		const module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		if (!(moduleId in __webpack_modules__)) {
+/******/ 			delete __webpack_module_cache__[moduleId];
+/******/ 			const e = new Error("Cannot find module '" + moduleId + "'");
+/******/ 			e.code = 'MODULE_NOT_FOUND';
+/******/ 			throw e;
+/******/ 		}
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	// define getter/value functions for harmony exports
+/******/ 	__webpack_require__.d = (exports, definition) => {
+/******/ 		for(var key in definition) {
+/******/ 			if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 				Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 			}
+/******/ 		}
+/******/ 	};
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	__webpack_require__.o = (obj, prop) => (Object.hasOwn(obj, prop));
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = (exports) => {
+/******/ 		Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/ 	
+/************************************************************************/
+let __webpack_exports__ = {};
+// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
+(() => {
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _modules_modal__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/modal */ "./src/js/modules/modal.js");
+/* harmony import */ var _modules_sliders__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/sliders */ "./src/js/modules/sliders.js");
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+
+  (0,_modules_modal__WEBPACK_IMPORTED_MODULE_0__["default"])('.button-design', '.popup-design', '.popup-design .popup-close');
+  (0,_modules_modal__WEBPACK_IMPORTED_MODULE_0__["default"])('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
+  (0,_modules_modal__WEBPACK_IMPORTED_MODULE_0__["default"])('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true);
+  (0,_modules_sliders__WEBPACK_IMPORTED_MODULE_1__["default"])('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
+  (0,_modules_sliders__WEBPACK_IMPORTED_MODULE_1__["default"])('.main-slider-item', 'vertical');
+});
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
---
+++ src/js/modules/forms.js
@@ -0,0 +1,178 @@
+
+const forms = () => {
+  const formsArr = document.querySelectorAll('form'),
+        uploadInpArr = document.querySelectorAll('[name="upload"]');
+
+  const statusMessages = {
+    loading: 'Загрузка...',
+    success: 'Данные успешно отправлены',
+    failure: 'Произошла ошибка',
+    spinner: 'assets/img/spinner.gif',
+    ok: 'assets/img/ok.png',
+    fail: 'assets/img/fail.png',
+  }
+
+  const pathApi = {
+    design: 'assets/design.php',
+    consult: 'assets/server.php'
+  }
+
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: 'POST',
+      body: data
+    });
+
+    return await res.text();
+  }
+
+  uploadInpArr.forEach(input => {
+    input.addEventListener("input", e => {
+      if (validateUpload(input)) {
+        const fileName = e.target.files[0].name.split('.');
+        let dots = fileName[0].length < 6 ? '.' : '...';
+        input.previousElementSibling.textContent = fileName[0].slice(0, 6) + dots + fileName[1];
+      }   
+    });
+  })
+
+  formsArr.forEach(form => {
+    const reqInputs = form.querySelectorAll('[required]');
+
+    reqInputs.forEach(inp => {
+      inp.addEventListener('input', () => {
+        if (inp.value.trim() !== '') {
+          removeError(inp);
+        }
+      }); 
+    });
+
+    form.addEventListener("submit", e => {
+      e.preventDefault();
+      
+      if(!validateForm(form)) {
+        return;
+      }
+
+      form.classList.add('animated', 'fadeOutUp');
+      form.style.position = 'absolute';
+      form.style.left = '0';
+
+      setTimeout(() => {
+        form.style.display = 'none';
+      }, 400);
+
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.classList.add('animated', 'fadeInUp');
+      form.parentNode.append(statusMessage);
+
+      const img = document.createElement('img');
+      img.setAttribute('src', statusMessages.spinner);
+      statusMessage.append(img);
+
+      const textMessage = document.createElement('div');
+      textMessage.textContent = statusMessages.loading;
+      statusMessage.append(textMessage);
+
+      const formData = new FormData(form);
+      let api;
+      form.classList.contains('form_design') ? api = pathApi.design : api = pathApi.consult;
+
+      postData(api, formData)
+        .then(res => {
+          console.log(res);
+          textMessage.textContent = statusMessages.success;
+          img.setAttribute('src', statusMessages.ok);
+        })
+        .catch(error => {
+          console.log(error);
+          img.setAttribute('src', statusMessages.fail)
+          textMessage.textContent = statusMessages.failure;
+        })
+        .finally(() => {
+          form.reset();
+
+          uploadInpArr.forEach(input => {
+            input.previousElementSibling.textContent = 'Файл не выбран';
+          });
+
+          setTimeout(() => {
+            statusMessage.remove();
+            form.classList.add('fadeInDown');
+            form.classList.remove('fadeOutUp');
+            form.style.display = 'block';
+            form.style.position = 'relative'; 
+          }, 5000);
+        });
+    });
+  });
+
+}
+
+function showError(element) {
+  element.classList.add('error');
+  const errorMessage = document.createElement('div');
+  errorMessage.classList.add('error-message');
+
+  errorMessage.textContent =
+    element.dataset.error ||
+    'Заполните это поле';
+
+  element.insertAdjacentElement('afterend', errorMessage);
+}
+
+function removeError(element) {
+  element.classList.remove('error');
+
+  const errorMessage = element.nextElementSibling;
+
+  if (errorMessage?.classList.contains('error-message')) {
+    errorMessage.remove();
+  }
+}
+
+function validateForm (form) {
+  const reqInputs = form.querySelectorAll('[required]');
+  let isValid = true;
+
+  reqInputs.forEach(inp => {
+    removeError(inp);
+
+    if (inp.value.trim() === '') {
+      showError(inp);
+      isValid = false;
+    } 
+  });
+
+  return isValid;
+}
+
+function validateUpload(inp) {
+  console.log(inp.files[0]);
+  const file = inp.files[0];
+  let isValid = true;
+
+  if (!file) {
+    showError(inp);
+    return isValid = false;
+  }
+
+  const maxSize = 2 * 1024 * 1024;
+  if (file.size > maxSize) {
+    alert('Файл слишком большой! Максимальный размер — 2 МБ.');
+    inp.value = '';
+    return isValid = false;
+  }
+
+  const allowedTypes = ['image/jpeg', 'image/png', 'application/pdf'];
+  if (!allowedTypes.includes(file.type)) {
+    alert('Недопустимый формат файла.');
+    inp.value = '';
+    return isValid = false;
+  }
+
+  return isValid;
+}
+
+export default forms;
